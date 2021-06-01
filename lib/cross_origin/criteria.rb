15584fe58a955b724ad357ab8aa2686d9dc353fb
Maikel Arcia Carrazana <macarci@gmail.com>
2021-05-31 22:26:11 -0400

    Update | Cross origin insert test

--- lib/cross_origin/criteria.rb
+++ lib/cross_origin/criteria.rb
@@ -18,8 +18,10 @@ module CrossOrigin
         doc['origin'] = origin
         docs << doc
       end
-      klass.with(persistence_context) do |klass_with_persistence_context|
-        (cross_origin&.collection_for(klass_with_persistence_context) || klass_with_persistence_context.collection).insert_many(docs) unless docs.empty?
+      unless docs.empty?
+        klass.with(persistence_context) do |klass_with_persistence_context|
+          (cross_origin&.collection_for(klass_with_persistence_context) || klass_with_persistence_context.collection).insert_many(docs)
+        end
       end
       origins.each { |collection, ids| collection.find(_id: { '$in' => ids }).delete_many }
     end
